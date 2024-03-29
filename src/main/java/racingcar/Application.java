1804dce88f10b184164804379881d324bd54532a
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-03-29 14:15:25 +0900

    첫번째 커밋
    4번째 기능요구사항 완료
    
    4번째 기능요구사항인 사용자가 몇번의 이동을 할 것인지 입력을 할 수 있게 했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        //사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
+        int tryNum = Integer.parseInt(Console.readLine());
+        
+
+    }
+
+    private static class Car {
+        private String name;
+        private int location;
+
+        public Car(String name, int cnt) {
+            this.name = name;
+            this.location = location;
+        }
+
     }
 }
